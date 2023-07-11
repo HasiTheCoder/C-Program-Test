429cf97255416db221011173dc507c16bbbec6ed
HasiTheCoder <105689152+HasiTheCoder@users.noreply.github.com>
2023-07-11 16:16:14 +0500

    Add files via upload

---
+++ CSharp-TestProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CSharpTestProgram
+{
+    class program
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hi my name is Hasnain");
+            
+        }
+    }
+}
